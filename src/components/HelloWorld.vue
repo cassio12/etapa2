df454e86c3216ea6838e9442d76c4b0117e96abf
Vainawebbuser <cassioprazeres241@gmail.com>
2018-01-20 15:41:35 -0200

    etapa02

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="menu">
+    <div class="mune-inicio">inicio</div>
+    <div class="menu-sobre">sobre</div>
+    <div class="menu-contato">contato</div>
+    <div class="container-gato">
+      <img class="container-gato-img" src="../assets/gato.jpg">
+    </div>
+    <div>
+      <h1>Dedesafio</h1>
+      <p>Vai Na Web EAD</p>
+    </div>
+    <div class="container-paragrafo">
+      <P>O Vai Na Web é um curso de desenvolvimento de páninas web e cidadânia dentro</P>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
